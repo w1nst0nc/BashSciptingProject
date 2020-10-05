ec07533ac10960f0014f53a321c637f5b471ea5e
w1nst0nc <71241979+w1nst0nc@users.noreply.github.com>
2020-10-04 20:23:28 -0500

    Add files via upload

---
+++ backup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+user=$(whoami)
+input=/c/Users/$user
+output=/tmp/${user}_home_$(date +%Y-%m-%d_%H%M%S).tar.gz
+
+tar -czf $output $input
+echo "Backup of $input completed! Details about the output backup file:"
+ls -1 $output
