7975238d784a70166cdeed7417a12d8eb9ce28dd
w1nst0nc <71241979+w1nst0nc@users.noreply.github.com>
2020-10-04 23:29:43 -0500

    Add files via upload

--- backup.sh
+++ backup.sh
@@ -1,9 +1,63 @@
 #!/bin/bash
 
-user=$(whoami)
+function backup
+ {
+   if [ -z $1 ]; then
+   user=$(whoami)
+   else
+    if [ ! -d "/home/$1" ]; then
+     echo "Requested $1 user home directory doesn't exist."
+     exit 1
+    fi
+    user=$1
+   fi
+
 input=/c/Users/$user
 output=/tmp/${user}_home_$(date +%Y-%m-%d_%H%M%S).tar.gz
 
-tar -czf $output $input
-echo "Backup of $input completed! Details about the output backup file:"
-ls -1 $output
+function total_files
+ {
+   find $1 -type f | wc -1
+ }
+
+function total_directories
+ {
+   find $1 -type d | wc -1
+ }
+
+function total_archived_directories
+ {
+   tar -tzf $1 | grep /$ | wc -1
+ }
+
+function total_archived_files
+ {
+   tar -tzf $1 | grep -v /$ | wc -1
+ }
+
+tar -czf $output $input 2> /dev/null
+
+src_files=$( total_files $input )
+src_directories=$( total_directories $input )
+
+arch_files=$( total_archived_files $output )
+arch_directories=$( total_archived_directories $input )
+
+echo "########## $user ##########"
+
+echo "Files to be included: $src_files"
+echo "Directories to be included: $src_directories"
+echo "Files archived: $arch_files"
+echo "Directories archived: $arch_directories"
+
+if [ $src_files -eq $arch_files ]; then
+  echo "Backup of $input completed!"
+  echo "Details about the output backup file:"
+  ls -1 $output
+else
+   echo "Backup of $input failed!"
+fi
+ }
+for directory in $*; do
+   backup $directory
+done;
